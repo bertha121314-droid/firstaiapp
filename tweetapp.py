13f834e4fddaf7fd88af7d99005ce7c0ca7c1d8e
bertha121314-droid <bertha121314@gmail.com>
2025-09-10 13:49:45 -0400

    Update tweetapp.py

--- tweetapp.py
+++ tweetapp.py
@@ -16,18 +16,19 @@ import os
 os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
 
 # Create prompt template for generating tweets
-tweet_template = "Give me {number} tweets on {topic}"
+tweet_template = "Give me {number} funny tweets on {topic}"
 tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic'])
 # Create LLM chain using the prompt template and model
 tweet_chain = tweet_prompt | gemini_model | StrOutputParser()
 # print(tweet_chain.invoke({"number" : 2, "topic" : "Wars in Middle East"}))
 
+import streamlit as st
 
-
-st.header("Tweet Generator")
+st.header("😃 :red[Funny] :blue[Tweet Generator]")
 st.subheader("Generate Tweets using Generative AI")
 topic = st.text_input("Topic")
 number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1, step = 1)
 if st.button("Generate"):
+    st.subheader("Funny Tweets")
     tweets = tweet_chain.invoke({"number" : number, "topic" : topic})
     st.write(tweets)
